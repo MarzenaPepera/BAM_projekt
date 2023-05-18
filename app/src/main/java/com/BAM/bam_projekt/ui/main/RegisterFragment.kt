d357acb71221795d23130ca8859c33eea6e143fe
Marzena <marzena.pepera@gmail.com>
2023-05-18 13:26:05 +0200

    storzenie projektu i ekran rejestracji (wypisanie danych w logach)

--- app/src/main/java/com/BAM/bam_projekt/ui/main/RegisterFragment.kt
+++ app/src/main/java/com/BAM/bam_projekt/ui/main/RegisterFragment.kt
@@ -38,6 +38,7 @@ class RegisterFragment : Fragment() {
 
             Log.d("RegisterFragment", "Username: $username, Password: $password")
 
+
             viewModel.registerUser(username, password)
         }
     }
