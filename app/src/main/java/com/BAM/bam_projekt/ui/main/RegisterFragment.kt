323e952b74b8108e6b6e824effcfdc6ab6ade444
Marzena <marzena.pepera@gmail.com>
2023-05-18 23:10:16 +0200

    rejestracja i dodanie Firebase

--- app/src/main/java/com/BAM/bam_projekt/ui/main/RegisterFragment.kt
+++ app/src/main/java/com/BAM/bam_projekt/ui/main/RegisterFragment.kt
@@ -9,11 +9,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.BAM.bam_projekt.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterFragment : Fragment() {
 
-    private lateinit var viewModel: RegisterViewModel
+    //private lateinit var viewModel: RegisterViewModel
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +31,8 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
+        auth = Firebase.auth
+        //viewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
 
         // Przykładowe zdarzenie kliknięcia przycisku rejestracji
         val registerButton = view.findViewById<Button>(R.id.button_register)
@@ -38,7 +45,29 @@ class RegisterFragment : Fragment() {
 
             Log.d("RegisterFragment", "Username: $username, Password: $password")
 
-            viewModel.registerUser(username, password)
+            //viewModel.registerUser(username, password)
+            registerNewUser(username, password)
+
         }
+    }
+    private fun registerNewUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Rejestracja udana, aktualizuj interfejs użytkownika z danymi użytkownika
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    // Możesz teraz przekierować użytkownika do innego fragmentu lub aktywności
+                } else {
+                    // Jeśli rejestracja nie powiedzie się, wyświetl komunikat dla użytkownika.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(context, "Rejestracja nie powiodła się.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    companion object {
+        private const val TAG = "RegisterFragment"
     }
 }
